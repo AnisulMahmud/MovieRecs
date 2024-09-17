5d85920096c597ecacedfa5c0c51ff6ecf6c66ec
Anisul Mahmud <anisul.mahmud@outlook.com>
2024-09-17 14:10:47 +0300

    database connected

--- dev-backend/src/main/java/com/anis/dev_backend/DevBackendApplication.java
+++ dev-backend/src/main/java/com/anis/dev_backend/DevBackendApplication.java
@@ -2,12 +2,21 @@ package com.anis.dev_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+
 public class DevBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevBackendApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot() {
+		return "Hello World";
 	}
 
 }
